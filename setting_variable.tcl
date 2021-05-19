c0413b32e909e765cfefd2ea92f4d2f0e80943bb
ayengec <83873798+ayengec@users.noreply.github.com>
2021-05-19 23:32:16 +0300

    Added comments

--- setting_variable.tcl
+++ setting_variable.tcl
@@ -4,5 +4,12 @@ set strVar "The value of double variable is"
 set dblVar 1.24
 set intVar 10
 
-puts "$strVar $dblVar and the integer variable equals to $intVar" 
+puts "$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new line"    
+# \n and \t usages: new line and tab usage are similar to C/C++ 
 # with $ and the variable name means the value of that variable
+
+###################### OUTPUT #############################################
+# ******* ayengec TCL example: assigning variables and printing *******
+# The value of double variable is 1.24 	the integer variable equals to 10. 
+# Here is new line
+###########################################################################
