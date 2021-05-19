d5d08e52f677eb649a1018d50d85fcff3b5c54ed
ayengec <83873798+ayengec@users.noreply.github.com>
2021-05-19 22:12:33 +0300

    Basic example of tcl variable, sets, puts

---
+++ setting_variable.tcl
@@ -0,0 +1,8 @@
+puts "******* ayengec TCL example: assigning variables and printing *******"
+# set variableName(any type) Value_of_variable
+set strVar "The value of double variable is"
+set dblVar 1.24
+set intVar 10
+
+puts "$strVar $dblVar and the integer variable equals to $intVar" 
+# with $ and the variable name means the value of that variable
