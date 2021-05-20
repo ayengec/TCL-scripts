fb378230fdc24ccf06c014a54b1b75f4a667cd8f
ayengec <83873798+ayengec@users.noreply.github.com>
2021-05-20 10:03:42 +0300

    Update setting_variable.tcl

--- setting_variable.tcl
+++ setting_variable.tcl
@@ -9,9 +9,10 @@ puts "$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new li
 # with $ and the variable name means the value of that variable
 
 # if you don't use any variable in puts expression, you can also use between {}. But whole chars will be printed as you see below.
-puts {$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new line}
+
 ###################### OUTPUT #############################################
 # ******* ayengec TCL example: assigning variables and printing *******
 # The value of double variable is 1.24 	the integer variable equals to 10. 
 # Here is new line
+# puts {$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new line}
 ###########################################################################
