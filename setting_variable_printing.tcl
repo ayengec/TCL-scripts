a0db4ab09f2022975ff3ac65ff974ce79a4a2ce8
ayengec <83873798+ayengec@users.noreply.github.com>
2021-05-20 10:09:11 +0300

    Update setting_variable_printing.tcl

--- setting_variable_printing.tcl
+++ setting_variable_printing.tcl
@@ -9,10 +9,15 @@ puts "$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new li
 # with $ and the variable name means the value of that variable
 
 # if you don't use any variable in puts expression, you can also use between {}. But whole chars will be printed as you see below.
+puts {$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new line}
 
-###################### OUTPUT #############################################
+set dblVar [set intVar 55]
+puts "new intVar = $intVar and nested dblVar = $dblVar"
+
+###################### OUTPUTS #############################################
 # ******* ayengec TCL example: assigning variables and printing *******
 # The value of double variable is 1.24 	the integer variable equals to 10. 
 # Here is new line
-# puts {$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new line}
+# $strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new line
+# new intVar = 55 and nested dblVar = 55
 ###########################################################################
