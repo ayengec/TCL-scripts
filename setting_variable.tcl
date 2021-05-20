fd47198f44fdd0aebb13ba621e93e7d0dcc8ce8e
ayengec <83873798+ayengec@users.noreply.github.com>
2021-05-20 10:03:01 +0300

    Updated with curly braces.

--- setting_variable.tcl
+++ setting_variable.tcl
@@ -8,6 +8,8 @@ puts "$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new li
 # \n and \t usages: new line and tab usage are similar to C/C++ 
 # with $ and the variable name means the value of that variable
 
+# if you don't use any variable in puts expression, you can also use between {}. But whole chars will be printed as you see below.
+puts {$strVar $dblVar \tthe integer variable equals to $intVar. \nHere is new line}
 ###################### OUTPUT #############################################
 # ******* ayengec TCL example: assigning variables and printing *******
 # The value of double variable is 1.24 	the integer variable equals to 10. 
