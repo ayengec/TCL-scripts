5c629714ad8f2804e4da9b4d1afefa2e75a61551
ayengec <83873798+ayengec@users.noreply.github.com>
2021-05-21 11:23:48 +0300

    Create proc_example.tcl

---
+++ proc_example.tcl
@@ -0,0 +1,27 @@
+  
+# ayengec TCL script examples: proc
+# proc (procedur) is similar to "function" "def" defines in other languages.
+# it gets input argumants and returns output value that processed.
+
+proc divider {val div} {
+    if { $div == 0 } {
+        puts "You cannot give div=0 Try again."
+        return "ZERO DIVISION ERROR! ENDLESS"
+    } else {
+    set y [expr {$val/$div}];
+    return $y
+    }
+}
+
+set result [divider 2 0]       # second example [divider 120 24]
+puts "The result = $result"
+
+################### OUTPUT #######################
+# You cannot give div=0 Try again.
+# The result = ZERO DIVISION ERROR! ENDLESS
+
+############While [divider 120 24] OUTPUT ########
+# The result = 5
+##################################################
+
+
